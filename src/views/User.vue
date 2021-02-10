1607a329f1af7e2e0c9b927f3b02e2ba2240f051
李祥捷 <773983534@qq.com>
2021-02-10 17:58:28 +0800

    添加User.vue

--- src/views/User.vue
+++ src/views/User.vue
@@ -1,10 +1,44 @@
 <template>
-
+  <div>
+    <table>
+      <tr>
+        <th>Id</th>
+        <th>Name</th>
+        <th>Password</th>
+      </tr>
+      <tr v-for="user in users">
+        <td>{{ user.id }}</td>
+        <td>{{ user.name }}</td>
+        <td>{{ user.password }}</td>
+      </tr>
+    </table>
+  </div>
 </template>
 
 <script>
 export default {
-name: "User"
+  name: "User",
+  data() {
+    return {
+      users: [
+        {
+          id: 1,
+          name: 'lxj1',
+          password: '123456'
+        },
+        {
+          id: 2,
+          name: 'lxj2',
+          password: '123456'
+        },
+        {
+          id: 3,
+          name: 'lxj3',
+          password: '123456'
+        }
+      ]
+    }
+  }
 }
 </script>
 
